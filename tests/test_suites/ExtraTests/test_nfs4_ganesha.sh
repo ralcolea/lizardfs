d3341c973a5ca7a25e7d261754977cdf62ada167
Antuan <antuan@lizardfs.org>
2022-12-07 22:41:53 -0500

    tests: Update ganesha test to add cthon test suite.

--- tests/test_suites/ExtraTests/test_nfs4_ganesha.sh
+++ tests/test_suites/ExtraTests/test_nfs4_ganesha.sh
@@ -10,6 +10,7 @@ CHUNKSERVERS=1 \
 	setup_local_empty_lizardfs info
 
 mkdir -p ${TEMP_DIR}/mnt/ganesha
+mkdir -p ${TEMP_DIR}/mnt/ganesha/cthon_test
 mkdir -p ${info[mount0]}/data
 mkdir -p ${info[mount0]}/etc/ganesha
 mkdir -p ${info[mount0]}/lib/ganesha
@@ -88,6 +89,61 @@ test_error_cleanup() {
 sudo ${info[mount0]}/bin/ganesha.nfsd -f ${info[mount0]}/etc/ganesha/ganesha.conf
 sudo mount -vvvv localhost:/data $TEMP_DIR/mnt/ganesha
 
+cat <<EOF > "${TEMP_DIR}/mnt/ganesha/cthon04.patch"
+diff --git a/lock/tlock.c b/lock/tlock.c
+index 8c837a8..7060cca 100644
+--- a/lock/tlock.c
++++ b/lock/tlock.c
+@@ -479,21 +479,21 @@ fmtrange(offset, length)
+
+ #ifdef LARGE_LOCKS                     /* non-native 64-bit */
+        if (length != 0)
+-               sprintf(buf, "[%16llx,%16llx] ", offset, length);
++               sprintf(buf, "[%16lx,%16lx] ", offset, length);
+        else
+-               sprintf(buf, "[%16llx,          ENDING] ", offset);
++               sprintf(buf, "[%16lx,          ENDING] ", offset);
+ #else /* LARGE_LOCKS */
+        if (sizeof (offset) == 4) {
+                if (length != 0)
+-                       sprintf(buf, "[%8lx,%8lx] ", (int32_t)offset,
++                       sprintf(buf, "[%i,%i] ", (int32_t)offset,
+                                (int32_t)length);
+                else
+-                       sprintf(buf, "[%8lx,  ENDING] ", (int32_t)offset);
++                       sprintf(buf, "[%i,  ENDING] ", (int32_t)offset);
+        } else {
+                if (length != 0)
+-                       sprintf(buf, "[%16llx,%16llx] ", offset, length);
++                       sprintf(buf, "[%16lx,%16lx] ", offset, length);
+                else
+-                       sprintf(buf, "[%16llx,          ENDING] ", offset);
++                       sprintf(buf, "[%16lx,          ENDING] ", offset);
+        }
+ #endif /* LARGE_LOCKS */
+
+EOF
+
+sudo -i -u lizardfstest bash << EOF
+ cd "${TEMP_DIR}/mnt/ganesha"
+ chmod o+w "$(pwd)"
+ if test -d "${TEMP_DIR}/mnt/ganesha/cthon04"; then
+    rm -rf "${TEMP_DIR}/mnt/ganesha/cthon04"
+ fi
+
+ git clone --no-checkout git://git.linux-nfs.org/projects/steved/cthon04.git
+ cd cthon04
+ git reset --hard HEAD
+ git apply --ignore-whitespace "${TEMP_DIR}/mnt/ganesha/cthon04.patch"
+# sudo chown -R lizardfstest:lizardfstest $TEMP_DIR/mnt/ganesha/cthon04
+ make all
+# git status
+ export NFSTESTDIR=${TEMP_DIR}/mnt/ganesha/cthon_test
+ ./runtests -b -n
+ ./runtests -l -n
+ ./runtests -s -n
+EOF
+
 cd ${TEMP_DIR}/mnt/ganesha
 MAX_FILES=300
 ### Check mkdir / rmdir syscall
@@ -109,8 +165,8 @@ assert_equals ${GROUP_ID} "$(echo "${STATS_REPORT}" | grep -i gid | cut -d/ -f3 
 assert_equals ${GANESHA_BS} "$(echo "${STATS_REPORT}" | grep -i 'io block' | cut -d: -f4 | awk '{print $1}')"
 
 ### Check readdir / ls|tree / syscall
-assert_equals ${MAX_PATH_DEPH} $(tree ${TEMP_DIR}/mnt/ganesha | grep directories | awk '{print $1}')
-assert_equals "$((${MAX_FILES} + 1))" "$(ls ${TEMP_DIR}/mnt/ganesha/ | wc -l)"
+assert_equals $((${MAX_PATH_DEPH} + 12)) $(tree ${TEMP_DIR}/mnt/ganesha | grep directories | awk '{print $1}')
+assert_equals "$((${MAX_FILES} + 3))" "$(ls ${TEMP_DIR}/mnt/ganesha/ | wc -l)"
 
 ### Check open2 / cat|dd / syscall
 assert_equals "Ganesha_Test_Ok" $(cat $(find -name file))
